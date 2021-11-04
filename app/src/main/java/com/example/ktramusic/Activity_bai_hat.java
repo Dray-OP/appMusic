6803a497a5e47ba9d0095a54d6e90a1b858613ec
Dray-OP <62054995+Dray-OP@users.noreply.github.com>
2021-11-04 14:31:57 +0700

    demo1

---
+++ app/src/main/java/com/example/ktramusic/Activity_bai_hat.java
@@ -0,0 +1,14 @@
+package com.example.ktramusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_bai_hat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai_hat);
+    }
+}
--- app/src/main/java/com/example/ktramusic/MainActivity.java
+++ app/src/main/java/com/example/ktramusic/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.ktramusic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView_casi;
+    ArrayList<list_casi> list_casi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView_casi = (ListView) findViewById(R.id.list_casi);
+
+        list_casi = new ArrayList<list_casi>();
+        list_casi.add(new list_casi("bài 1",""));
+        list_casi.add(new list_casi("bài 2",""));
+        list_casi.add(new list_casi("bài 3",""));
+        list_casi.add(new list_casi("bài 4",""));
+        list_casi.add(new list_casi("bài 5",""));
+        list_casi.add(new list_casi("bài 6",""));
+
+        Adapter_casi adapter_casi = new Adapter_casi(MainActivity.this,R.layout.list_music,list_casi);
+
+        listView_casi.setAdapter(adapter_casi);
+
     }
 }
